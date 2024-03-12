3f79d37270c450a160a9976cd6554553a686c88a
Melody <meliaukea@gmail.com>
2024-03-11 20:55:34 -0400

    Took out the code for SSL authentication

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,25 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: 'localhost',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    logging: true
   }
 );
+
+// Test the connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });   
+
 
 module.exports = sequelize;
